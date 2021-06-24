c19e121ef0be6dad960d7127d6b202e25e9ed87e
ABS Hasan <49762426+Learner0112@users.noreply.github.com>
2021-06-24 19:00:30 +0800

    Add files via upload

---
+++ RPA-Customer-Segmentation-main/README.md
@@ -0,0 +1,25 @@
+# RPA-Customer-Segmentation
+
+![image](https://user-images.githubusercontent.com/49762426/123249924-13a03880-d51c-11eb-99c8-043b6d7053e9.png)
+
+
+
+1) The marketing department wants to send a Born in the ‘80s email to the appropriate users.
+Find the email addresses and birthdays of users whose birthday is between 1980-01-01 and 1989-12-31.
+
+2) We are interested in thegroup of users that signed up prior to May 2017.
+Find the emails and creation date of users whose created_at date matches this condition.
+
+3) There was an A/B test performed on users that used cute animal clipart to encourage users to sign up. We’d like to see how the group that was shown ‘bears’ is performing.
+Find the emails of the users who received the ‘bears’ test.
+
+4) A total of 4 advertising campaigns were run over this period.
+There were two sets of ad copy (set 1 and set 2) and both were run on two search engine sites (AAA and BBB).
+Find all the emails of all users who received a campaign on website BBB.
+
+5) Find all the emails of all users who received ad copy 2 in their campaign.
+
+6) Find the emails for all users who received both a campaign and a test.
+These users will have non-empty entries in the campaign and test columns.
+
+7) how old users were when they signed up.
---
+++ RPA-Customer-Segmentation-main/RPA-Customer-Segmentation.sql
@@ -0,0 +1,44 @@
+-- 1
+-- What are the column names?
+SELECT * FROM users ;
+ 
+-- 2
+-- Find the email addresses and birthdays of users whose 
+-- birthday is between 1980-01-01 and 1989-12-31.
+SELECT email, birthday from users where birthday between '1980-01-01' AND '1989-12-31'  ;
+   
+-- 3
+-- Find the emails and creation date of users 
+-- whose created_at date matches this condition.
+select email, created_at from users WHERE created_at < '2017-05-01' ;
+
+-- 4
+-- Find the emails of the users who received the ‘bears’ test.
+SELECT email FROM users where test = 'bears'  ;
+
+-- 5
+-- Find all the emails of all users who 
+-- received a campaign on website BBB.
+
+SELECT email FROM users where campaign LIKE 'BBB%'  ;
+
+-- 6
+-- Find all the emails of all users who received ad copy 2 in 
+-- their campaign.
+SELECT email FROM users WHERE campaign like '%-2' ; 
+
+-- 7
+-- Find the emails for all users who received both a campaign and a test. 
+-- These users will have non-empty entries in the 
+-- campaign and test columns.
+SELECT email FROM users WHERE campaign is not null and test IS NOT NULL ;
+GO
+-- 8
+-- Challenge
+-- One of the members of the marketing team had an idea of calculating
+-- how old users were when they signed up.
+
+SELECT  DATEDIFF(YEAR,  birthday, created_at) as age FROM users;
+
+
+
